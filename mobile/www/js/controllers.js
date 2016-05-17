d712e1769f94a6058d0b7ec3de4bf25788527da6
Andrei TV <a.talviy@gmail.com>
2016-05-17 13:57:42 +0200

    modularisation des features

--- mobile/www/js/controllers.js
+++ mobile/www/js/controllers.js
@@ -1,14 +1,6 @@
 angular.module('app.controllers', [])
-  
-.controller('cameraTabDefaultPageCtrl', function($scope) {
 
-})
-   
-.controller('cartTabDefaultPageCtrl', function($scope) {
-
-})
-   
 .controller('cloudTabDefaultPageCtrl', function($scope) {
 
 })
-    
+
--- mobile/www/js/routes.js
+++ mobile/www/js/routes.js
@@ -1,49 +1,43 @@
-angular.module('app.routes', [])
+angular.module('app.routes', ['app.around', 'app.tour'])
 
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-    
-  
-
+  .config(function ($stateProvider, $urlRouterProvider) {
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
       .state('tabsController.cameraTabDefaultPage', {
-    url: '/page2',
-    views: {
-      'tab1': {
-        templateUrl: 'templates/cameraTabDefaultPage.html',
-        controller: 'cameraTabDefaultPageCtrl'
-      }
-    }
-  })
+        url: '/page2',
+        views: {
+          'tab1': {
+            templateUrl: 'templates/around-me.html',
+            controller: 'aroundMeCtrl'
+          }
+        }
+      })
 
-  .state('tabsController.cartTabDefaultPage', {
-    url: '/page3',
-    views: {
-      'tab2': {
-        templateUrl: 'templates/cartTabDefaultPage.html',
-        controller: 'cartTabDefaultPageCtrl'
-      }
-    }
-  })
+      .state('tabsController.tourPage', {
+        url: '/page3',
+        views: {
+          'tab2': {
+            templateUrl: 'templates/tour-page.html',
+            controller: 'tourCtrl'
+          }
+        }
+      })
 
-  .state('cloudTabDefaultPage', {
-    url: '/page4',
-    templateUrl: 'templates/cloudTabDefaultPage.html',
-    controller: 'cloudTabDefaultPageCtrl'
-  })
+      .state('cloudTabDefaultPage', {
+        url: '/page4',
+        templateUrl: 'templates/cloudTabDefaultPage.html',
+        controller: 'cloudTabDefaultPageCtrl'
+      })
 
-  .state('tabsController', {
-    url: '/page1',
-    templateUrl: 'templates/tabsController.html',
-    abstract:true
-  })
+      .state('tabsController', {
+        url: '/page1',
+        templateUrl: 'templates/tabsController.html',
+        abstract: true
+      })
 
-$urlRouterProvider.otherwise('/page1/page2')
+    $urlRouterProvider.otherwise('/page1/page2')
 
-  
-
-});
+  });

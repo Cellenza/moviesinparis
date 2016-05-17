678a88a1009e397335fd814d2bc2cfac9ac7caf1
lnimong <lnimong@gmail.com>
2016-05-17 14:28:45 +0200

    tour

--- MoviesInParis/src/mobile/www/js/tour/tour.js
+++ MoviesInParis/src/mobile/www/js/tour/tour.js
@@ -1,7 +1,28 @@
 angular.module('app.tour', [])
-  .controller('tourCtrl', function ($scope) {
+  .controller('tourCtrl', function ($scope, tourService) {
+    
+    $scope.loadTour = function (theme) {
+      tourService.getTour(them).then(function() {
+        
+        scenes.forEach(function (s) {
+          //onverra
+        })
+      })
+    }
 
   })
   .service('tourService', function ($http) {
     
+    this.getTour = function (theme) {
+
+      var defer = $q.defer();
+
+      $http.get('http://scenecity.azurewebsites.net/api/AroundMe/10/10/test')
+        .then(function (res) {
+
+          defer.resolve(res.data);
+        });
+      return defer.promise;
+
+    }
   })
